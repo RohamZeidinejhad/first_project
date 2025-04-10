ee69d4ce907df8f7ad50c337452377a54a071a76
Roham Zeidinejhad <roham.program@gmail.com>
2025-04-10 14:45:24 +0330

    Main Activity Toast Added

---
+++ app/src/main/java/com/example/github/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.github
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show()
+
+
+    }
+}
